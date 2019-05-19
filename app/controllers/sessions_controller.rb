f2da5c6f15aa622ab88478699fe637413d2528f0
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-05-19 20:42:11 +0900

    ログインボタンを置いてslack認証画面にリンクした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    @authorize_url = Authentication.authorize_url(callback_url)
   end
 
   def create
--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -4,4 +4,13 @@ class Authentication < ApplicationRecord
   validates :slack_workspace_id, presence: true
   validates :slack_user_id, presence: true, uniqueness: { scope: :slack_workspace_id }
   validates :access_token, presence: true
+
+  def self.authorize_url(callback_url)
+    params = {
+      scope: 'identity.basic,identity.avatar',
+      client_id: ENV['SLACK_CLIENT_ID'],
+      redirect_uri: callback_url
+    }
+    "https://slack.com/oauth/authorize?#{params.to_query}"
+  end
 end

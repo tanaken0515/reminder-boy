30429c2eeb10e8a6790e19c68f0b9776269e6713
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-05-20 22:44:39 +0900

    rootは一旦ログイン画面にした

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   get '/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'sessions#new'
 end

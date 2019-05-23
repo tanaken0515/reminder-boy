7b9ce9396d6ec9064cdf3414cf21bee767f3325c
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-05-23 23:28:43 +0900

    docker-compose run --rm web rails g controller reminders index new show edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'reminders/index'
+  get 'reminders/new'
+  get 'reminders/show'
+  get 'reminders/edit'
   get '/login', to: 'sessions#new'
   get '/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'

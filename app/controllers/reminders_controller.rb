aec4dc0c293e36780c9680a2804564042ce1ea21
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-05-24 22:42:48 +0900

    チャンネルのリストをインスタンス変数にした

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -4,6 +4,7 @@ class RemindersController < ApplicationController
 
   def new
     @reminder = current_user.reminders.new
+    @slack_channel_list = [%w(ch1_name ch1_id), %w(ch2_name ch2_id), %w(ch3_name ch3_id)] #todo: apiで取ってくる
   end
 
   def show

e9197c1be3ec9e616ebb061460c8b4f1e08ec659
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-05-25 10:23:28 +0900

    booleanのカラムに presence: ture を指定するとfalseで保存できなくなるからダメ - ちなみにtrue,falseだけに絞りたければ inclusion: {in: [true, false]} をつけると良い - でも何もつけない方が '1' -> true, '0' -> false などの変換をしてくれるので便利っぽい

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -5,14 +5,5 @@ class Reminder < ApplicationRecord
   validates :message, presence: true
   validates :hour, presence: true
   validates :minute, presence: true
-  validates :holiday_included, presence: true
   validates :status, presence: true
-
-  validates :monday_enabled, presence: true
-  validates :tuesday_enabled, presence: true
-  validates :wednesday_enabled, presence: true
-  validates :thursday_enabled, presence: true
-  validates :friday_enabled, presence: true
-  validates :saturday_enabled, presence: true
-  validates :sunday_enabled, presence: true
 end

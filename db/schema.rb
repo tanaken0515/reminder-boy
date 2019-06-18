a301e1907d4eecb29d2638c87054d84f8780d06c
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-06-18 22:31:38 +0900

    RemindLogにemoji_urlカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_012009) do
+ActiveRecord::Schema.define(version: 2019_06_18_132912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_06_09_012009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slack_permalink"
+    t.string "emoji_url"
     t.index ["reminder_id"], name: "index_remind_logs_on_reminder_id"
   end
 

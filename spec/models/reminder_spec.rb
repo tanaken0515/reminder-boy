ca1401ef876ca360110782713812400216bd0f08
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-06-29 18:12:38 +0900

    spec追加: 各scopeのspecを追加した

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -75,29 +75,113 @@ RSpec.describe Reminder, type: :model do
     end
   end
 
-  xdescribe 'scopes' do
+  describe 'scopes' do
+    let(:user) { create(:user_with_authentication) }
     before do
-      'create some reminders'
+      allow(user).to receive(:slack_client).and_return(slack_client_mock)
     end
 
     describe 'from_latest' do
+      before do
+        create(:reminder, message: 'a', user: user)
+        create(:reminder, message: 'b', user: user)
+        create(:reminder, message: 'c', user: user)
+        create(:reminder, message: 'd', user: user)
+        create(:reminder, message: 'e', user: user)
+      end
 
+      it '新しい順になっていること' do
+        expect(Reminder.from_latest.pluck(:message)).to eq %w(e d c b a)
+      end
     end
 
     describe 'holiday_included' do
+      before do
+        create(:reminder, message: 'a', holiday_included: false, user: user)
+        create(:reminder, message: 'b', holiday_included: true, user: user)
+        create(:reminder, message: 'c', holiday_included: true, user: user)
+      end
 
+      it '祝日通知ONのデータだけ取得されること' do
+        expect(Reminder.holiday_included.pluck(:message)).to match_array %w(b c)
+      end
     end
 
     describe 'enabled_on_day_of_week' do
+      before do
+        create(:reminder, message: 'a', sunday_enabled: true, monday_enabled: true, user: user)
+        create(:reminder, message: 'b', sunday_enabled: false, monday_enabled: true, user: user)
+        create(:reminder, message: 'c', sunday_enabled: true, monday_enabled: false, user: user)
+        create(:reminder, message: 'd', sunday_enabled: false, monday_enabled: true, user: user)
+        create(:reminder, message: 'e', sunday_enabled: true, monday_enabled: false, user: user)
+      end
 
+      it '日曜(wday=0)のデータだけ取得されること' do
+        expect(Reminder.enabled_on_day_of_week(0).pluck(:message)).to match_array %w(a c e)
+      end
+
+      it '月曜(wday=1)のデータだけ取得されること' do
+        expect(Reminder.enabled_on_day_of_week(1).pluck(:message)).to match_array %w(a b d)
+      end
+
+      context '想定外の引数(0..6以外)が渡された場合' do
+        it 'データが取得されないこと' do
+          expect(Reminder.enabled_on_day_of_week(7)).to be_empty
+        end
+      end
     end
 
     describe 'active_on_date' do
+      before do
+        create(:reminder, message: 'a', monday_enabled: true, holiday_included: true, user: user)
+        create(:reminder, message: 'b', monday_enabled: true, holiday_included: false, user: user)
+        create(:reminder, message: 'c', monday_enabled: false, holiday_included: true, user: user)
+        create(:reminder, message: 'd', monday_enabled: true, holiday_included: true, user: user)
+        create(:reminder, message: 'e', monday_enabled: false, holiday_included: false, user: user)
+      end
 
+      context '祝日でない月曜日(2019/7/8)の場合' do
+        it '月曜のデータが取得されること' do
+          expect(Reminder.active_on_date(Date.new(2019, 7, 8)).pluck(:message)).to match_array %w(a b d)
+        end
+      end
+
+      context '祝日の月曜日(2019/7/15)の場合' do
+        it '月曜かつ祝日ONのデータが取得されること' do
+          expect(Reminder.active_on_date(Date.new(2019, 7, 15)).pluck(:message)).to match_array %w(a d)
+        end
+      end
     end
 
     describe 'scheduled_between' do
+      before do
+        create(:reminder, message: 'a', scheduled_time: '07:00', user: user)
+        create(:reminder, message: 'b', scheduled_time: '15:00', user: user)
+        create(:reminder, message: 'c', scheduled_time: '23:00', user: user)
+      end
+      let(:reminders) { Reminder.scheduled_between(from, to) }
 
+      let(:from) { Time.zone.parse('2019-07-01 07:00:00') }
+      let(:to) { Time.zone.parse('2019-07-01 15:00:00') }
+      it '範囲内のデータが取得されること' do
+        expect(reminders.pluck(:message)).to match_array %w(a b)
+      end
+
+      context '範囲が日付を跨いでいるために、時刻だけみると大小関係が逆転している場合' do
+        let(:from) { Time.zone.parse('2019-07-01 23:00:00') }
+        let(:to) { Time.zone.parse('2019-07-02 07:00:00') }
+        it '正しく取得されること' do
+          expect(reminders.pluck(:message)).to match_array %w(a c)
+        end
+      end
+
+      context '引数の大小関係が逆転している場合' do
+        let(:from) { Time.zone.parse('2019-07-02 23:00:00') }
+        let(:to) { Time.zone.parse('2019-07-01 07:00:00') }
+        it 'データが取得されないこと' do
+          expect(reminders).to be_empty
+        end
+      end
     end
   end
 

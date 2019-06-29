3c624b59617c77e782055485e52b8b157e926e31
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-06-29 14:13:35 +0900

    テスト項目を追加

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Reminder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  xdescribe 'scopes' do
+    before do
+      'create some reminders'
+    end
+
+    describe 'from_latest' do
+
+    end
+
+    describe 'holiday_included' do
+
+    end
+
+    describe 'enabled_on_day_of_week' do
+
+    end
+
+    describe 'active_on_date' do
+
+    end
+
+    describe 'scheduled_between' do
+
+    end
+  end
+
+  xdescribe '#enabled_day_of_weeks' do
+
+  end
+
+  xdescribe '#everyday?' do
+
+  end
+
+  xdescribe '#weekday?' do
+
+  end
+
+  xdescribe '#holiday?' do
+
+  end
+
+  xdescribe '#post' do
+
+  end
+
+  xdescribe '#remind!' do
+
+  end
 end

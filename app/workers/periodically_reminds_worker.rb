19993c046ce2c752ce06a9792d99614640463597
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-06-30 11:29:38 +0900

    タイムゾーンごとに実行時点の日付を判定するようにした

--- app/workers/periodically_reminds_worker.rb
+++ app/workers/periodically_reminds_worker.rb
@@ -7,9 +7,16 @@ class PeriodicallyRemindsWorker
     to = Time.zone.now
     from = to - (interval - 1).seconds  # 09:10:00 に実行された場合、この値は 09:05:01
 
-    reminder_ids = Reminder.active_on_date(Time.zone.today).scheduled_between(from, to).pluck(:id)
-    reminder_ids.each do |reminder_id|
-      RemindWorker.perform_async(reminder_id)
+    reminders_by_time_zone = Reminder.scheduled_between(from, to)
+                               .includes(:user).group_by{|reminder| reminder.user.time_zone}
+
+    reminders_by_time_zone.each do |time_zone, reminders|
+      Time.use_zone(time_zone) do
+        reminder_ids = Reminder.where(id: reminders.pluck(:id)).active_on_date(Time.zone.today).pluck(:id)
+        reminder_ids.each do |reminder_id|
+          RemindWorker.perform_async(reminder_id)
+        end
+      end
     end
   end
 end

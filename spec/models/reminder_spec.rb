1908ab0a751440f2fb8ae7e8621f34bd9cefcd4b
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-06-30 09:47:24 +0900

    refactoring: SlackAPIクライアントをmock化する処理を共通化した

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,34 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Reminder, type: :model do
-  let(:slack_client_mock) do
-    double('Slack Client',
-           conversations_list: conversations_list_response,
-           chat_postMessage: chat_postMessage_response,
-           chat_getPermalink: chat_getPermalink_response)
-  end
-
-  let(:conversations_list_response) do
-    channels = [
-      Slack::Messages::Message.new(id: 'C*****1', name: 'ch1', is_archived: false),
-      Slack::Messages::Message.new(id: 'C*****2', name: 'ch2', is_archived: false),
-      Slack::Messages::Message.new(id: 'C*****3', name: 'ch3', is_archived: true),
-      Slack::Messages::Message.new(id: 'C*****4', name: 'ch4', is_archived: false),
-    ]
-
-    Slack::Messages::Message.new(ok: true, channels: channels)
-  end
-
-  let(:chat_postMessage_response) do
-    icons = Slack::Messages::Message.new(emoji: ':simple_smile:')
-    message = Slack::Messages::Message.new(icons: icons)
-
-    Slack::Messages::Message.new(ok: true, ts: '1234567890.001000', message: message)
-  end
-
-  let(:chat_getPermalink_response) do
-    Slack::Messages::Message.new(ok: true, permalink: 'https://*****.slack.com/archives/C*****/p*****')
-  end
+  include_context 'make_slack_client_mock_available'
 
   describe '#valid?' do
     let(:user) { create(:user_with_authentication) }

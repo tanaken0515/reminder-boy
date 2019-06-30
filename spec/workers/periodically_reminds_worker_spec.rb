0b1d42050abf12b43c2807cfd5209859d98aa714
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-06-30 11:47:31 +0900

    time_zoneメソッドのmock化をやめてカラムを見るようにした

--- spec/workers/periodically_reminds_worker_spec.rb
+++ spec/workers/periodically_reminds_worker_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe PeriodicallyRemindsWorker, type: :worker do
   include_context 'make_slack_client_mock_available'
 
-  let(:user01) { create(:user_with_authentication) }
-  let(:user02) { create(:user_with_authentication) }
+  let(:user01) { create(:user_with_authentication, time_zone: 'UTC') }
+  let(:user02) { create(:user_with_authentication, time_zone: 'Asia/Tokyo') }
 
   before do
     allow(user01).to receive(:slack_client).and_return(slack_client_mock)
-    allow(user01).to receive(:time_zone).and_return('UTC')
     Time.use_zone(user01.time_zone) do
       @reminder_at_2300_utc = create(:reminder, monday_enabled: true, scheduled_time: '23:00', user: user02)
       @reminder_at_0000_utc = create(:reminder, monday_enabled: true, scheduled_time: '00:00', user: user02)
@@ -16,7 +15,6 @@ RSpec.describe PeriodicallyRemindsWorker, type: :worker do
     end
 
     allow(user02).to receive(:slack_client).and_return(slack_client_mock)
-    allow(user02).to receive(:time_zone).and_return('Asia/Tokyo')
     Time.use_zone(user02.time_zone) do
       @reminder_at_0800_jp = create(:reminder, monday_enabled: true, scheduled_time: '08:00', user: user01)
       @reminder_at_0900_jp = create(:reminder, monday_enabled: true, scheduled_time: '09:00', user: user01)

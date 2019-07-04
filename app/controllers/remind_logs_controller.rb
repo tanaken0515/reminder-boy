5f9bf2b5d2c9e38efdc37a5259ebfa47ef1c699d
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-07-04 23:13:36 +0900

    RemindLogをページングできるようにした

--- app/controllers/remind_logs_controller.rb
+++ app/controllers/remind_logs_controller.rb
@@ -2,6 +2,6 @@ class RemindLogsController < ApplicationController
   include RemindLogsHelper
 
   def index
-    @remind_logs = current_user.remind_logs.from_latest
+    @remind_logs = current_user.remind_logs.from_latest.page(params[:page]).per(5)
   end
 end
